63c1f9f7980cf31f8de171bac88867587d004c2b
Cooper Benson <cooper@amprobotics.com>
2019-08-28 13:11:53 -0600

    Attempted schematic

---
+++ projects/ngx-c3-lib/src/lib/ngx-c3-lib.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import { ChartComponent } from './chart.component';
+
+
+@NgModule({
+  declarations: [ChartComponent],
+  imports: [],
+  exports: []
+})
+export class NgxC3LibModule {
+}
---
+++ projects/ngx-c3-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-c3-lib
+ */
+
+export * from './lib/chart.component';
+export * from './lib/ngx-c3-lib.module';
